6f6ec0bb21f1150d25803a4dc677704003e90f95
kumarmadhan30242 <kumarmadhan30242@gmail.com>
2025-09-01 16:46:26 +0530

    add all files

---
+++ one/src/assets/Components/Oneee.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Oneee = () => {
+  return (
+    <>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos dicta suscipit obcaecati, ullam fugiat corrupti. Totam explicabo aliquid maxime quo neque ea! Hic velit atque soluta nemo, ab obcaecati dolor?</p>
+    </>
+  )
+}
+
+export default Oneee
